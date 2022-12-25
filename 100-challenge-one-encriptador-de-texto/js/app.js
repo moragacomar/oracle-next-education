eac432e4bad5ef69094b423d64488d6b5fcdf128
Juan Moraga <poluxfc@gmail.com>
2022-12-25 18:07:13 -0300

    format js

--- 100-challenge-one-encriptador-de-texto/js/app.js
+++ 100-challenge-one-encriptador-de-texto/js/app.js
@@ -1,8 +1,11 @@
-const eliminarAcentos = (texto) => texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+const eliminarAcentos = (texto) =>
+    texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
 const encriptar = () => {
-    const texto = eliminarAcentos(document.getElementById("texto").value.toLowerCase());
-    const msjEncriptado = document.querySelector(".sidebar")
+    const texto = eliminarAcentos(
+        document.getElementById("texto").value.toLowerCase()
+    );
+    const msjEncriptado = document.querySelector(".sidebar");
     const textoCodificado = texto.split("").map((el) => {
         if (el === "a") return "ai";
         else if (el === "e") return "enter";
@@ -13,12 +16,12 @@ const encriptar = () => {
     });
     msjEncriptado.style.textAlign = "left";
     msjEncriptado.style.justifyContent = "space-between";
-    msjEncriptado.innerHTML = `<p class="mensajeIngreseTexto">${textoCodificado.join("")}</p><input class="btnCopiar" type="submit" value="Copiar" />`;
+    msjEncriptado.innerHTML = `<p class="mensajeIngreseTexto">${textoCodificado.join(
+        ""
+    )}</p><input class="btnCopiar" type="submit" value="Copiar" />`;
     document.querySelector(".btnCopiar").addEventListener("click", copiar, false);
+};
 
-
-
-}
 const copiar2 = () => {
     var dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
@@ -26,22 +29,34 @@ const copiar2 = () => {
     dummy.select();
     document.execCommand("copy");
     document.body.removeChild(dummy);
-}
+};
 function copiar() {
     var x = document.getElementById("snackbar");
     x.className = "show";
-    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function () {
+        x.className = x.className.replace("show", "");
+    }, 3000);
 }
 const desencriptar = () => {
-    const textoCodificado = eliminarAcentos(document.getElementById("texto").value.toLowerCase());
-    const textoNormal = textoCodificado.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
-    const msjEncriptado = document.querySelector(".sidebar")
+    const textoCodificado = eliminarAcentos(
+        document.getElementById("texto").value.toLowerCase()
+    );
+    const textoNormal = textoCodificado
+        .replace(/ai/g, "a")
+        .replace(/enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ober/g, "o")
+        .replace(/ufat/g, "u");
+    const msjEncriptado = document.querySelector(".sidebar");
     msjEncriptado.style.textAlign = "left";
     msjEncriptado.style.justifyContent = "space-between";
     msjEncriptado.innerHTML = `<p class="mensajeIngreseTexto">${textoNormal}</p><input class="btnCopiar" type="submit" value="Copiar" />`;
     document.querySelector(".btnCopiar").addEventListener("click", copiar, false);
-}
+};
 
-document.querySelector(".btnEncriptar").addEventListener("click", encriptar, false);
-document.querySelector(".btnDesencriptar").addEventListener("click", desencriptar, false);
-
+document
+    .querySelector(".btnEncriptar")
+    .addEventListener("click", encriptar, false);
+document
+    .querySelector(".btnDesencriptar")
+    .addEventListener("click", desencriptar, false);
